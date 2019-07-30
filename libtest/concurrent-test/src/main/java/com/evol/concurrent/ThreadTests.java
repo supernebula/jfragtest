6589a385cb5c5408ab61a97dc92498e72d09a909
supernebula <supernebula@126.com>
2019-07-30 22:07:02 +0800

    init

---
+++ libtest/concurrent-test/src/main/java/com/evol/concurrent/ThreadTests.java
@@ -0,0 +1,15 @@
+package com.evol.concurrent;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.Test;
+
+//public class ThreadTests {
+//
+//    private final Calculator calculator = new Calculator();
+//
+//    @Test
+//    void addition() {
+//        assertEquals(2, calculator.add(1, 1));
+//    }
+//
+//}
