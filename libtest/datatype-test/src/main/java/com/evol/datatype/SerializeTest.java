48a2b74b8e3a57bae12cea19228a503bc7849ab8
caozhongqiang <caozhongqiang@hzchangchong.com>
2019-09-30 16:35:55 +0800

    xml serialize ok

--- libtest/datatype-test/src/main/java/com/evol/datatype/SerializeTest.java
+++ libtest/datatype-test/src/main/java/com/evol/datatype/SerializeTest.java
@@ -3,6 +3,8 @@ package com.evol.datatype;
 import com.alibaba.fastjson.JSON;
 import com.evol.datatype.model.GenderEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.evol.datatype.model.SerialUser;
@@ -72,6 +74,42 @@ public class SerializeTest {
         }catch (Exception ex){
             System.out.println(ex);
         }
+    }
+
+    @Test
+    public void objectToXmlTest(){
+        SerialUser user = createUser();
+        user.setName("zhanshan");
+        user.setAge(18);
+        user.setCreateTime(new Date());
+        user.setEnabled(true);
+        user.setMoney(100.01);
+        user.setGender(GenderEnum.MALE);
+        XStream xStream = new XStream(new DomDriver());
+
+        xStream.alias("SerialUser", SerialUser.class);
+        String xml = xStream.toXML(user);
+        System.out.println(xml);
+
+    }
+
+    @Test
+    public void xmlToObjTest(){
+
+        String xml = "<SerialUser>\n" +
+                "  <name>zhanshan</name>\n" +
+                "  <age>18</age>\n" +
+                "  <money>100.01</money>\n" +
+                "  <enabled>true</enabled>\n" +
+                "  <actived>false</actived>\n" +
+                "  <gender>MALE</gender>\n" +
+                "  <createTime>2019-09-30 08:28:20.558 UTC</createTime>\n" +
+                "</SerialUser>";
+
+        XStream xStream = new XStream(new DomDriver());
+        xStream.alias("SerialUser", SerialUser.class);
+        SerialUser user = (SerialUser)xStream.fromXML(xml);
+        System.out.print(user);
     }
 
 
